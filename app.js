060419760ca96e72511644f313fda651e4930209
shekh farid <shekhfarid1974@gmail.com>
2024-07-06 14:22:31 +0600

    javascript t-1

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log('Hello');
+
+document.getElementById('title').innerHTML = 'Hello world';
+
+let sum = 150+40;
+console.log(sum);
+
+if (sum > 100) {
+    console.log('Expensive');
+
+}else{
+    console.log('Cheep');
+}
+
+function sayHello(name) {
+    console.log(`Hello ${name}`);
+}
+sayHello('Farid');
